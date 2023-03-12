70a9847e4067baffc1ed9f476edf74f35addd1ee
sukabing <2113097922@qq.com>
2023-03-12 19:30:39 +0800

    update

--- WebsiteApp/static/js/javascript.js
+++ WebsiteApp/static/js/javascript.js
@@ -4,15 +4,15 @@ function getCSS(){
 	var timenow=datetoday.getTime();
 	datetoday.setTime(timenow);
 	var thehour = datetoday.getHours();
-	if (thehour<12)
+	if (thehour>0 && thehour<12)
 		display = "bg-0.png";
-	else if (thehour>12)
+	else if (thehour>12 && thehour < 18)
 	//样式表的名称，或者你可以加上路径
-		display = "bg-1.png";
-	else
 		display = "bg-2.png";
+	else if (thehour > 18 && thehour < 24)
+		display = "bg-1.png";
 	//(...想要更多再加即可...)
 	
 	var css ='<style type="text/css">body{background-image: url("../static/img/'+display+'");background-size: cover;}</style>';
-	document.write(css);
+	document.write(css+thehour);
 }
