85a9b11ae5508f1e9eba98834a43713e0d234bef
sukabing_ <2113097922@qq.com>
2023-03-26 17:21:31 +0800

    update

--- Website/settings.py
+++ Website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0b4__@aby9i^)7=hxr9mmnzkv0url@1u&d^8f%2&jwvw9uwat#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
