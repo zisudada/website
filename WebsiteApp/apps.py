c02ed6c4ed5145718b0d725d7cba1c22500426d3
sukabing <2113097922@qq.com>
2023-04-14 17:58:37 +0800

    update

--- WebsiteApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebsiteappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'WebsiteApp'
--- WebsiteApp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class UserInfo(models.Model):
-    user_name = models.CharField(max_length=64)
-    user_password = models.CharField(max_length=64)
